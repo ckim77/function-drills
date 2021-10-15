8f4330d270037f252c4216cbe68ade93cf6944de
Chris Kim <ckjr92@gmail.com>
2021-10-15 13:47:00 -0600

    Got problem 15 to finallty work

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -270,7 +270,25 @@ let sampleArray = [0,1,2,3,4,7,5,6,8,9]
   Write a function that takes in an array of numbers as an argument. In the body of the function, write logic to determine if the array is in ascending order. The function should return true, if it is sorted in ascending order, false if it is not. Create a variable, `arrayIsAscending` and set it equal to your function invoked. Use the sample array to test this function.
 */
 
-//CODE HERE
+function ascendingOrNah(arr) {
+  let res = [];
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[i + 1]){
+      res.push('f');
+    } else {
+      res.push('t');
+    }
+  }
+  if (res.includes('f')) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+let lst = [1,2,4,6,6,3]
+let arrayIsAscending = ascendingOrNah(lst);
+console.log(arrayIsAscending);
 
 
 ////////////////// PROBLEM 16 ////////////////////
